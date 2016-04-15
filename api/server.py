ad70994f0f2028cc132921265600c6f788005893
Brandon Liu <bdon@bdon.org>
2016-04-15 11:13:59 +0800

    Formatting and status endpoint

--- api/server.py
+++ api/server.py
@@ -8,25 +8,29 @@ from lxml import etree
 
 height_db = task.height_db()
 
+@app.route("/")
+def status():
+  return "OK"
+
 #16,10490,25317
 @app.route("/sfbuildingheight_<int:z>_<int:x>_<int:y>.osm")
 def changeset(z,x,y):
-    if z < 16:
-      print "Too big"
-      raise Exception
-    bb = mercantile.bounds(x,y,z)
-    bb = [str(f) for f in [bb.west,bb.south,bb.east,bb.north]]
-    url = "http://openstreetmap.org/api/0.6/map?bbox=" + ','.join(bb)
-    osm_api_response = requests.get(url)
-    changeset = task.changeset(BytesIO(osm_api_response.content),height_db)
+  if z < 16:
+    print "Too big"
+    raise Exception
+  bb = mercantile.bounds(x,y,z)
+  bb = [str(f) for f in [bb.west,bb.south,bb.east,bb.north]]
+  url = "http://openstreetmap.org/api/0.6/map?bbox=" + ','.join(bb)
+  osm_api_response = requests.get(url)
+  changeset = task.changeset(BytesIO(osm_api_response.content),height_db)
 
-    filename = "sfbuildingheight_{0}_{1}_{2}.osm".format(z,x,y)
-    return Response(etree.tostring(changeset,pretty_print=True),
-      mimetype="text/xml",
-      headers={
-      'Content-Disposition':'attachment; filename={0}'.format(filename)
-    })
+  filename = "sfbuildingheight_{0}_{1}_{2}.osm".format(z,x,y)
+  return Response(etree.tostring(changeset,pretty_print=True),
+    mimetype="text/xml",
+    headers={
+    'Content-Disposition':'attachment; filename={0}'.format(filename)
+  })
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+  app.debug = True
+  app.run()
