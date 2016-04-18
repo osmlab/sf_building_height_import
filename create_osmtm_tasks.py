fa5e5d70077198bc0ad4db4699e78c47b9038a2f
Brandon Liu <bdon@bdon.org>
2016-04-18 20:33:54 +0800

    fix y of tile coordinates

--- create_osmtm_tasks.py
+++ create_osmtm_tasks.py
@@ -7,9 +7,8 @@ from shapely.geometry import mapping
 
 conn = psycopg2.connect(dbname="us.ca.san_francisco")
 cur = conn.cursor()
-cur.execute("select ST_AsBinary(bounds_for_tile_indices(y+1, x, z)), x,y,z as geom from tasks order by x, y;")
+cur.execute("select ST_AsBinary(bounds_for_tile_indices(y, x, z)), x,y as y,z as geom from tasks order by x, y;")
 
-task_id = 1
 features = []
 for result in cur:
   x = result[1]
@@ -22,6 +21,5 @@ for result in cur:
     'id':name,
     'geometry':mapping(loads(str(result[0])))
   })
-  task_id = task_id + 1
 
 print json.dumps({'type':'FeatureCollection','features':features})
