8cd5d2853f054c56f32b3c4e0405b13077760798
Brandon Liu <bdon@bdon.org>
2016-04-18 19:54:06 +0800

    task import_url for task manager ui

--- create_osmtm_tasks.py
+++ create_osmtm_tasks.py
@@ -12,13 +12,16 @@ cur.execute("select ST_AsBinary(bounds_for_tile_indices(y+1, x, z)), x,y,z as ge
 task_id = 1
 features = []
 for result in cur:
+  x = result[1]
+  y = result[2]
+  z = result[3]
+  name = "{0}_{1}_{2}.osm".format(z,x,y)
   features.append({
     'type':'Feature',
-    'properties':{'x':result[1],'y':result[2],'z':result[3]},
-    'id':task_id,
+    'properties':{'import_url':"http://tiles.openmassing.org/api/sfbuildingheight_" + name},
+    'id':name,
     'geometry':mapping(loads(str(result[0])))
   })
   task_id = task_id + 1
 
 print json.dumps({'type':'FeatureCollection','features':features})
-
